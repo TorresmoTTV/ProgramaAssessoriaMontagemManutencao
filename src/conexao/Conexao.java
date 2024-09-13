32b374730fcd55f52b82b27eacd93192a5db0d8a
TorresmoTTV <grand_turismo8@hotmail.com>
2024-09-12 21:50:11 -0300

    Update 1.2

--- src/conexao/Conexao.java
+++ src/conexao/Conexao.java
@@ -1,5 +1,21 @@
 package conexao;
 
-public class Conexao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Conexao {
+    private static String host = "jdbc:mysql://localhost:3306/ProgramaAsMonMa";
+    private static String user = "root";
+    private static String pass = "";
+
+    public static Connection getConexao() throws SQLException{
+        Connection c = null;
+        try {
+            c = DriverManager.getConnection(host, user, pass);
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao conectar!\n" + e.getMessage());
+        }
+        return c;
+    }
 }
---
+++ src/model/User.java
@@ -0,0 +1,7 @@
+package model;
+
+public class User {
+    private int idusuario, CPF, telefone;
+    private String nome, email, endreco, usuario, senha;
+    
+}
